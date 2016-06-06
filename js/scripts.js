7b312fea387d89ab83788c234ba5c366446e12b1
Shatha Shaath and Shradha Pulla <shatha@>
2016-06-06 13:54:33 -0700

    Fix syntax error is js script, added s

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,17 @@
 $(document).ready(function() {
   $("form#content").submit(function(event) {
-   var favThings=["fav1","fav2","fav3"];
-   favThings.forEach(function(favThing){
-     var userinput=$("input#"+favThing).val();
-     $("#"+favThing).text(userinput);
+    var favThings = ["fav1","fav2","fav3","fav4","fav5"];
 
+    favThings.forEach(function(favThing) {
+      var userInput = $("input#" + favThing).val();
+      $("#" + favThing + "result").text(userInput);
     });
+    var newFav = [favThings[1], favThings[0], favThings[2]];
+
+    var newThing = favThings[0,1,2,3,4] += newFav[0,1,2];
+    alert(newThing);
 
     $("#results").show();
     event.preventDefault();
-
- });
+  });
 });
