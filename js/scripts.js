cb78739698dac98f6a605f663bacc8b4d104eb7a
Shatha Shaath and Shradha Pulla <shatha@>
2016-06-06 11:15:29 -0700

    Redo js code to loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,14 @@
 $(document).ready(function() {
   $("form#content").submit(function(event) {
-    var firstFav = $("input#fav1").val();
-    var secondFav = $("input#fav2").val();
-    var thirdFav = $("input#fav3").val();
+   var favThings=["fav1","fav2","fav3"];
+   favThings.forEach(function(favThing){
+     var userinput=$("input#"+favThing).val();
+     $("#"+favThing).text(userinput);
 
-    var favThings = [firstFav, secondFav, thirdFav];
-      alert(favThings);
+    });
 
-  event.preventDefault();
+    $("#results").show();
+    event.preventDefault();
 
  });
 });
